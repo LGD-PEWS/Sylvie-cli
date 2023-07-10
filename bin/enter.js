1e891ca0230ef6c4172f1075db9e62b6439409e6
LGD-PEWS <70420674+LGD-PEWS@users.noreply.github.com>
2023-07-10 15:30:01 +0800

    Add files via upload

---
+++ bin/enter.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+// 入口文件
+
+const program = require("commander");
+const packageJson = require("../package.json");
+
+program.version(packageJson.version);
+
+program
+  // 定义命令和参数
+  .command("create <app-name>")
+  .description("create a new project") // 添加描述信息
+  .option("-f, --force", "overwrite target directory if it exists") // 强制覆盖
+  .action((name, options) => {
+    // 执行该命令
+    require("../lib/create")(name, options);
+    // 打印执行结果
+    // console.log("name:", name, "options:", options);
+  });
+
+program.parse(process.argv);
